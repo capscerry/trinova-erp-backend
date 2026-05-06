666b9dab51d1863502285f8e2e95c8c7f9140b3b
vikoadrian32 <vikosilaban1@gmail.com>
2026-05-06 19:51:56 +0700

    Create ServiceExtensions.cs

---
+++ Config/ServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿using trinova_erp_backend.Repositories.Penjualan;
+using trinova_erp_backend.Usecase.Penjualan;
+
+namespace trinova_erp_backend.Config
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this  IServiceCollection services)
+        {
+            services.AddScoped<ICategoryCustomerRepo, CategoryCustomerRepo>();
+            services.AddScoped<IMasterCustomerRepo,MasterCustomerRepo>();
+
+            services.AddScoped<ICustomerUsecase, CustomerUsecase>();
+            return services;
+        }
+    }
+}
