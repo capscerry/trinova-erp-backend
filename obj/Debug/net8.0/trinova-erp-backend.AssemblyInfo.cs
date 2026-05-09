eaa40c2e90e5463d09aa88d420e50c848d4951ba
vikoadrian32 <vikosilaban1@gmail.com>
2026-05-09 18:25:59 +0700

    add gitignore

--- obj/Debug/net8.0/trinova-erp-backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/trinova-erp-backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("trinova-erp-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4c9bdd85d5b2bb0c0ae4707cad2bd7194e27df7")]
 [assembly: System.Reflection.AssemblyProductAttribute("trinova-erp-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("trinova-erp-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
