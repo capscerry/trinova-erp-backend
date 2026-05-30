684307f04feeb4b6af8c1b7a911147eec3aac64a
Devita <devitasetyanirafi12@gmail.com>
2026-05-30 18:05:21 +0700

    chore: sync feature inventory with development update

--- obj/Debug/net8.0/trinova-erp-backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/trinova-erp-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("trinova-erp-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c17cf48e338a81d10230bbcd38acd534937eb8d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+081bd7d5448d5780ee570f174b33842dc6640824")]
 [assembly: System.Reflection.AssemblyProductAttribute("trinova-erp-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("trinova-erp-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
