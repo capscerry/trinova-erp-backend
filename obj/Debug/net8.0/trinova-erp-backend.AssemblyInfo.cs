6e102ab0c6f0664293b2d81ac453aeabb43e6458
Devita <devitasetyanirafi12@gmail.com>
2026-07-03 09:10:39 +0700

    feat(inventory): improve stock transfer warehouse selection and auto-create destination stock

--- obj/Debug/net8.0/trinova-erp-backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/trinova-erp-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("trinova-erp-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a03880a6463bce1ed788107e8434d82fe7150cc2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c204f5b6a4e2850a9c88c76b83d9ce85f305e0d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("trinova-erp-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("trinova-erp-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
