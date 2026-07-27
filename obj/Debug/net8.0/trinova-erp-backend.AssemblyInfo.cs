add43dbe4d213bc4ba7f1b40a64211380a03e0c3
Ragil Maulana Ilyasha <ragilmaulanaa875@gmail.com>
2026-07-27 21:46:07 +0700

    merged

--- obj/Debug/net8.0/trinova-erp-backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/trinova-erp-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("trinova-erp-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a3bdfd95c9b222072f9cc7c3d80b056d6ce2b51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e041f55a6603786e0f574b887e56d996ced6ec67")]
 [assembly: System.Reflection.AssemblyProductAttribute("trinova-erp-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("trinova-erp-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
